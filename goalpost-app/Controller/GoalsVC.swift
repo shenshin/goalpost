45bb8c98cfba37ef33f71ec21f337dc2e8786373
Aleksandr Shenshin <shenshin@me.com>
2017-09-19 06:41:33 +0300

    настройка TableView

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreData
 
-class GoalsVC: UIViewController {
+class GoalsVC: UIViewController  {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var welcomeMessage: UIStackView!
@@ -18,6 +19,8 @@ class GoalsVC: UIViewController {
         super.viewDidLoad()
         self.tableView.isHidden = true
         self.welcomeMessage.isHidden = false
+        tableView.delegate = self
+        tableView.dataSource = self
       
     }
 
@@ -32,7 +35,40 @@ class GoalsVC: UIViewController {
             }
         }
     }
-    
-
 }
+
+//MARK: tableView инициализация
+//  забавный способ разделения класса на части, отвечающие за различные функции -  это создание расширения для класса с включением необходимой функции
+/*  Для работы TableView необходимо, чтобы класс соответствовал двум протоколам (UITableViewDelegate, UITableViewDataSource) и реализовывал как минимум три метода из этих протоколов (numberOfSections, numberOfRowsInSection, cellForRowAt indexPath), а также не забыть указать делегат и источник данных (tableView.delegate = self,  tableView.dataSource = self). Последнее делается в методе viewDidLoad()  */
+
+extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else {return UITableViewCell()}
+        cell.configureCell(description: "Кушай салат 2 раза в неделю", type: "Short Term", goalProgressAmount: 2)
+        return cell
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
